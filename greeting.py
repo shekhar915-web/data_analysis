dbc696dd21b6247e5663a82d375a2f9804f31074
shekhar915-web <mailtochandra915@gmail.com>
2026-03-22 19:34:28 +0000

    Create greeting.py

---
+++ greeting.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+@app.route("/")  # URL leading to method
+def hello():  # Name of the method
+    return "Hello World!"
+
+@app.route("/greetme")  # different URL
+def helloall():  # different method name
+    name = request.args.get('name')  # retrieve GET parameters
+    return "Hello {}!".format(name)  # Python’s string.format
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
